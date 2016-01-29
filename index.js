95e202e73579396b9d84cdaf913a0fe5984e9baf
Christoph Kisfeld <spam@b-web.org>
2016-01-29 03:11:03 +0100

    Update for latest versions of renderer and backend

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var exec = require('child_process').exec;
 var express = require('express');
 var app = express();
+var crypto = require('crypto');
 
 // folder where DirectionMaps-Backend, dirmaps-web and DirectionMaps-Rendering repos are located
-var home = "/home/c/code";
+var datahome = "/tmp/dirmapsdata";
+var dmalgdir = "/home/c/code/DirectionMaps-Backend/dist";
+var renderdir = "/home/c/code/pythonmapnik";
 
 app.get('/', function (req, res) {
 	res.send('Hello World!');
@@ -13,13 +16,23 @@ app.get('/map', function (req, res) {
 	//if(req.query.lat && req.query.lon){
 		var lat = parseFloat(req.query.lat) || '51.961831';
 		var lon = parseFloat(req.query.lon) || '7.617630';
-		var modes = ['car'];
+		var modes = ['car', 'bike'];
 		var mode = (modes.indexOf(req.query.lon) == -1) ? 'car' : req.query.mode;
+		var now = Date.now();
+		console.log(now);
+		var hash = crypto.createHash('md5').update(''+lat+lon+now).digest('hex');
 
-		console.log(lat, lon, mode);
+		console.log(lat, lon, mode, hash);
 
-		var a = runAlgos(lat, lon, mode, function(){
-			res.send('<img src="img/out.png">');
+		var options = {
+			"lat": lat,
+			"lon": lon,
+			"mode": mode,
+			"hash": hash
+		}
+
+		var a = runAlgos(options, function(){
+			res.send('<img src="img/'+options.hash+'.png">');
 		});
 		
 	//} else {
@@ -27,10 +40,10 @@ app.get('/map', function (req, res) {
 	//}
 });
 
-var runAlgos = function(lat, lon, mode, cb){
-	console.log('Running algorithms');
-	var child = exec('java -jar ../../DirectionMaps-Backend/dist/dm-alg.jar '+lat+' '+lon+' '+mode,
-		{ cwd: home+'/dirmaps-web/data' },
+var runAlgos = function(options, cb){
+	console.log('Calculating '+options.hash);
+	var child = exec('java -jar '+dmalgdir+'/dm-alg.jar '+options.lat+' '+options.lon+' '+options.mode+' '+options.hash,
+		{ cwd: datahome },
 		function (error, stdout, stderr) {
 			if (error !== null) {
 				console.log('exec error: ' + error);
@@ -38,31 +51,16 @@ var runAlgos = function(lat, lon, mode, cb){
 			}
 			console.log(stderr);
 			console.log(stdout);
-			runIntermediate(cb);
+			runRender(options, cb);
 			//return true;
 	});
 }
 
-var runIntermediate = function(cb){
-	console.log('Running intermediate');
-	var child = exec('python fetchattributes.py', 
-		{ cwd: home+'/pythonmapnik' },
-		function (error, stdout, stderr) {
-		if (error !== null) {
-			console.log('exec error: ' + error);
-			return false;
-		}
-		console.log(stderr);
-		console.log(stdout);
-		runRender(cb);
-		//return true;
-	});
-}
-
-var runRender = function(cb){
-	console.log('Running rendering');
-	var child = exec('python render.py', 
-		{ cwd: home+'/pythonmapnik' },
+var runRender = function(options, cb){
+	console.log('Rendering '+options.hash);
+	// python test.py 7.3828125 52.26815737376817 ./data output123.png asdf
+	var child = exec('python '+renderdir+'/render.py '+options.lat+' '+options.lon+' '+datahome+' "'+datahome+'/out/'+options.hash+'.png" '+options.hash,
+		{ cwd: renderdir },
 		function (error, stdout, stderr) {
 		if (error !== null) {
 			console.log('exec error: ' + error);
@@ -76,7 +74,7 @@ var runRender = function(cb){
 }
 
 
-app.use('/img', express.static('./data'));
+app.use('/img', express.static(datahome+'/out'));
 
 var server = app.listen(3000, function () {
 	var host = server.address().address;
