5e7c5d03934f55b347ec08bf93e81f1a3e6dd8fa
Christoph Kisfeld <spam@b-web.org>
2016-02-02 23:20:17 +0100

    - Return image as blob, fix #1 - Enable cors, fix #2 - Improved logging a bit - Travel mode default setting now works

--- index.js
+++ index.js
@@ -2,38 +2,55 @@ var exec = require('child_process').exec;
 var express = require('express');
 var app = express();
 var crypto = require('crypto');
+var fs = require('fs');
 
 // folder where DirectionMaps-Backend, dirmaps-web and DirectionMaps-Rendering repos are located
 var datahome = "/tmp/dirmapsdata";
 var dmalgdir = "/home/c/code/DirectionMaps-Backend/dist";
 var renderdir = "/home/c/code/pythonmapnik";
 
-app.get('/', function (req, res) {
-	res.send('Hello World!');
-});
+function log(msg) {
+	var thetime = new Date().toISOString();
+	console.log('['+ thetime +'] ', msg)
+}
+
+//app.get('/', function (req, res) {
+//	res.send('Direction Maps');
+//});
 
 app.get('/map', function (req, res) {
 	//if(req.query.lat && req.query.lon){
 		var lat = parseFloat(req.query.lat) || '51.961831';
 		var lon = parseFloat(req.query.lon) || '7.617630';
 		var modes = ['car', 'bike'];
-		var mode = (modes.indexOf(req.query.lon) == -1) ? 'car' : req.query.mode;
+		var mode = (modes.indexOf(req.query.mode) !== -1) ? req.query.mode : 'car';
 		var now = Date.now();
-		console.log(now);
 		var hash = crypto.createHash('md5').update(''+lat+lon+now).digest('hex');
 
-		console.log(lat, lon, mode, hash);
-
 		var options = {
+			"start": now,
 			"lat": lat,
 			"lon": lon,
 			"mode": mode,
 			"hash": hash
 		}
+		log(options);
 
-		var a = runAlgos(options, function(){
-			res.send('<img src="img/'+options.hash+'.png">');
-		});
+		res.header('Access-Control-Allow-Origin', '*');
+		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+		res.header('Access-Control-Allow-Headers', 'Content-Type');
+		try {
+			var a = runAlgos(options, function(){
+				var img = fs.readFileSync(datahome+'/out/'+options.hash+'.png');
+				res.writeHead(200, {'Content-Type': 'image/png' });
+				res.end(img, 'binary');
+				//res.send('<img src="img/'+options.hash+'.png">');
+				var timeElapsed = Date.now() - options.start;
+				log("" + options.hash + " took "+ Math.round(timeElapsed/1000) +" seconds")
+			});
+		} catch(e) {
+			res.send('{"msg":"An error occured"}');
+		}
 		
 	//} else {
 	//	res.send('Please specify latitude and longitude.');
@@ -41,40 +58,40 @@ app.get('/map', function (req, res) {
 });
 
 var runAlgos = function(options, cb){
-	console.log('Calculating '+options.hash);
+	log('Calculating '+options.hash);
 	var child = exec('java -jar '+dmalgdir+'/dm-alg.jar '+options.lat+' '+options.lon+' '+options.mode+' '+options.hash,
 		{ cwd: datahome },
 		function (error, stdout, stderr) {
 			if (error !== null) {
-				console.log('exec error: ' + error);
+				log('exec error: ' + error);
 				return false;
 			}
-			console.log(stderr);
-			console.log(stdout);
+			log(stderr);
+			log(stdout);
 			runRender(options, cb);
 			//return true;
 	});
 }
 
 var runRender = function(options, cb){
-	console.log('Rendering '+options.hash);
+	log('Rendering '+options.hash);
 	// python test.py 7.3828125 52.26815737376817 ./data output123.png asdf
 	var child = exec('python '+renderdir+'/render.py '+options.lat+' '+options.lon+' '+datahome+' "'+datahome+'/out/'+options.hash+'.png" '+options.hash,
 		{ cwd: renderdir },
 		function (error, stdout, stderr) {
 		if (error !== null) {
-			console.log('exec error: ' + error);
+			log('exec error: ' + error);
 			return false;
 		}
-		console.log(stderr);
-		console.log(stdout);
+		log(stderr);
+		log(stdout);
 		//return true;
 		cb();
 	});
 }
 
 
-app.use('/img', express.static(datahome+'/out'));
+app.use('/', express.static('./leaflet'));
 
 var server = app.listen(3000, function () {
 	var host = server.address().address;
